6677a05ae28596a2bbad62276108328c5de16509
benofcoding <22326@burnside.school.nz>
2024-05-08 13:50:40 +1200

    I made it print nice and turned it into a function

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
+
+
+#import
 import sqlite3
 
-db = sqlite3.connect("fighters.db")
-cursor = db.cursor()
-sql = "SELECT * FROM fighter;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close
+
+#constants/varibles
+DATABASE = "fighters.db"
+
+
+#functions
+def print_all_aircraft():
+    db = sqlite3.connect("fighters.db")
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop all results
+    print("Name           speed     Max_G     climb     range     payload ")
+    for fighter in results:
+        print(f"{fighter[1]:<15}{fighter[2]:<10}{fighter[3]:<10}{fighter[4]:<10}{fighter[5]:<10}{fighter[6]}")
+    
+    db.close
+
+
+#main code
+print_all_aircraft()
